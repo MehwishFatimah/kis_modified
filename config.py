ca7e2ae5785dbcff8831132a3e05b6108a6d0a45
mehwish Fatima <mehwishfatima.raja@gmail.com>
2022-04-14 12:01:54 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+model_card = "gpt2-medium"
+model_file = "/hits/basement/nlp/fatimamh/inputs/kis/models/gpt2_med_keep_it_simple.bin"
+
+data_dir = "/hits/basement/nlp/fatimamh/outputs/hipo/exp05"
+max_output_length=200
+num_runs=10
